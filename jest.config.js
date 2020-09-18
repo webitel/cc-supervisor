372d8aa86e5a9582eb04bc129a79248bde818119
dlohvinov <nsblnr@gmail.com>
2020-09-18 12:48:33 +0300

    test: added jest bamboo test parser [WTEL-1427]

--- jest.config.js
+++ jest.config.js
@@ -10,24 +10,34 @@ module.exports = {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/unit/mocks/fileMock.js',
     '\\.(css|less)$': '<rootDir>/tests/unit/mocks/fileMock.js',
   },
+  reporters: ['default', 'bamboo-jest-reporter'],
   collectCoverage: true,
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 80,
+  //     functions: 80,
+  //     statements: 80,
+  //     lines: 80,
+  //   },
+  // },
   collectCoverageFrom: [
-    // 'src/api/**/*.{js,vue}',
+    '!src/**/api/**/*.{js,vue}',
 
-    // 'src/components/queues/**/*.{js,vue}',
-    // 'src/components/active-calls/**/*.{js,vue}',
-    // 'src/components/agents/**/*.{js,vue}',
+    'src/components/queues/**/*.{js,vue}',
+    'src/components/active-calls/**/*.{js,vue}',
+    'src/components/agents/**/*.{js,vue}',
     'src/components/supervisor-workspace/**/*.{js,vue}',
 
-    // 'src/store/agents/**/*.{js,vue}',
-    // 'src/store/queues/**/*.{js,vue}',
-    // 'src/store/activeCalls/**/*.{js,vue}',
+    'src/store/agents/**/*.{js,vue}',
+    'src/store/queues/**/*.{js,vue}',
+    'src/store/activeCalls/**/*.{js,vue}',
     'src/store/userinfo/**/*.{js,vue}',
 
-    // 'src/shared/**/*.{js,vue}',
+    'src/shared/**/*.{js,vue}',
 
-    // 'src/mixins/**/*.{js,vue}',
+    'src/mixins/**/*.{js,vue}',
     '!src/**/index.js', // No need to cover bootstrap file
+    '!src/**/main.js', // No need to cover bootstrap file
   ],
   setupFiles: [
     './tests/config/config.js',
