44a90e20f0d47ed370291b4ec1843f2ddbbaa12b
dlohvinov <nsblnr@gmail.com>
2021-04-07 16:08:15 +0300

    refactor: added PageStoreModule, refactored queue and agentCalls modules with it [WTEL-1835]

--- src/modules/agents/modules/agent-page/modules/agent-calls/store/agent-calls.js
+++ src/modules/agents/modules/agent-page/modules/agent-calls/store/agent-calls.js
@@ -1,66 +1,19 @@
+import PageStoreModule from '../../../../../../../app/store/BaseStoreModules/PageStoreModule';
 import filters from './filters';
 import headers from './_internals/headers';
 import AgentCallsAPI from '../api/agent-calls';
 
 const state = {
   headers,
-  dataList: [],
-  isNext: false,
 };
 
 const getters = {
-  DATA_FIELDS: (state) => {
-    let fields = state.headers
-      .filter((header) => header.show)
-      .map((header) => header.field);
-    fields = [...new Set(fields)];
-    return fields;
-  },
-  GET_DATA_FIELDS_BY_VALUE: (state) => (headerValues) => state.headers
-    .reduce((fields, header) => {
-      if (headerValues.includes(header.value)) return fields.concat(header.field);
-      return fields;
-    }, []),
+  GET_FILTERS: (state, getters, rootState, rootGetters) => rootGetters['agents/agentPage/agentCalls/filters/GET_FILTERS'],
 };
 
-const actions = {
-  FETCH_LIST: async (context, params) => {
-    const fields = params.fields
-      ? context.getters.GET_DATA_FIELDS_BY_VALUE(params.fields)
-      : context.getters.DATA_FIELDS;
-    const filters = context.rootGetters['agents/agentPage/agentCalls/filters/GET_FILTERS'];
-    const _params = {
-      ...params,
-      ...filters,
-      fields,
-    };
-    const { items, next } = await AgentCallsAPI.getList(_params);
-    context.commit('SET_LIST', items);
-    context.commit('SET_IS_NEXT', { isNext: next });
-    return { items, next };
-  },
-  SET_HEADERS: (context, headers) => {
-    context.commit('SET_HEADERS', headers);
-  },
-};
-
-const mutations = {
-  SET_HEADERS: (state, headers) => {
-    state.headers = headers;
-  },
-  SET_LIST: (state, agents) => {
-    state.dataList = agents;
-  },
-  SET_IS_NEXT: (state, { isNext }) => {
-    state.isNext = isNext;
-  },
-};
-
-export default {
-  namespaced: true,
-  state,
-  getters,
-  actions,
-  mutations,
-  modules: { filters },
-};
+const agentCalls = new PageStoreModule({ state })
+  .setChildModules({ filters })
+  .attachAPIModule(AgentCallsAPI)
+  .generateAPIActions()
+  .getModule({ getters });
+export default agentCalls;
