91f418c5e4a0aab2abbaad15da5254a363c58f9e
dlohvinov <nsblnr@gmail.com>
2021-07-26 12:18:59 +0300

    refactor:  added navAccessMixin, refactored error pages with webitel-ui [WTEL-1946]

---
+++ src/app/mixins/supervisor-workspace/navAccessMixin.js
@@ -0,0 +1,28 @@
+import { mapGetters } from 'vuex';
+import SupervisorSections
+  from '@webitel/ui-sdk/src/enums/WebitelApplications/SupervisorSections.enum';
+import WebitelApplications
+  from '@webitel/ui-sdk/src/enums/WebitelApplications/WebitelApplications.enum';
+
+export default {
+  computed: {
+    ...mapGetters('userinfo', {
+      checkNavAccess: 'CHECK_OBJECT_ACCESS',
+    }),
+    nav() {
+      return [{
+        value: SupervisorSections.QUEUES,
+        name: this.$t(`WebitelApplications.${WebitelApplications.SUPERVISOR}.sections.${SupervisorSections.QUEUES}`),
+        route: '/queues',
+      }, {
+        value: SupervisorSections.AGENTS,
+        name: this.$t(`WebitelApplications.${WebitelApplications.SUPERVISOR}.sections.${SupervisorSections.AGENTS}`),
+        route: '/agents',
+      }, {
+        value: SupervisorSections.ACTIVE_CALLS,
+        name: this.$t(`WebitelApplications.${WebitelApplications.SUPERVISOR}.sections.${SupervisorSections.ACTIVE_CALLS}`),
+        route: '/active-calls',
+      }].filter((nav) => this.checkNavAccess({ name: nav.value }));
+    },
+  },
+};
