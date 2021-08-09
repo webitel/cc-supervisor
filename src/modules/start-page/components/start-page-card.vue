3519c9b9c8460dc3c06451811d6aacbe2437c1a9
Vasjamel <vasjanja@gmail.com>
2021-08-09 11:36:35 +0300

    feature: card updated [WTEL-1974]

--- src/modules/start-page/components/start-page-card.vue
+++ src/modules/start-page/components/start-page-card.vue
@@ -10,7 +10,7 @@
         {{ card.text }}
       </p>
     </section>
-    <footer>
+    <footer class="start-page-card__footer">
       <wt-button
           :disabled="card.disabled"
           wide
@@ -51,17 +51,28 @@ export default {
 
 .start-page-card__header {
   @extend %typo-heading-md;
+  display: flex;
+  //justify-content: center;
+  align-items: center;
+}
+
+.start-page-card__header i {
+  margin-right: var(--icon-spacing);
 }
 
 .start-page-card__description {
   @extend %typo-body-md;
   height: 42px;
-  margin: var(--component-spacing) 0;
+  //margin: var(--component-spacing) 0;
   //in order to cut text after 3rd line:
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
   -webkit-line-clamp: 3;
   -webkit-box-orient: vertical;
+}
+
+.start-page-card__footer {
+  margin-top: var(--component-spacing);
 }
 </style>
