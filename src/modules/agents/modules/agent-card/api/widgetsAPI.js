91b9b342ede1a8a83ef7c3bacf4b9cf458ec1b12
Lera24 <bedulinavalera21@gmail.com>
2023-12-15 17:26:32 +0200

    fix: widget API[WTEL-3725]

--- src/modules/agents/modules/agent-card/api/widgetsAPI.js
+++ src/modules/agents/modules/agent-card/api/widgetsAPI.js
@@ -1,31 +1,21 @@
 import { UserHelperServiceApiFactory } from 'webitel-sdk';
-import { SdkGetterApiConsumer } from 'webitel-sdk/esm2015/api-consumers';
-import applyTransform, { snakeToCamel } from '@webitel/ui-sdk/src/api/transformers';
+import applyTransform, { snakeToCamel, notify } from '@webitel/ui-sdk/src/api/transformers';
 import instance from '../../../../../app/api/instance';
 import configuration from '../../../../../app/api/utils/openAPIConfig';
 
 const userHelperService = new UserHelperServiceApiFactory(configuration, '', instance);
 
-const itemResponseHandler = (stats) => ({
-  ...stats,
-  scoreRequiredAvg: `${stats.scoreRequiredAvg.toFixed(2)}`,
-});
-
-const defaultSingleObject = {
-  scoreCount: 0,
-  scoreRequiredAvg: 0,
-};
-
-const getter = new SdkGetterApiConsumer(userHelperService.activityWorkspaceWidget, {
-  defaultSingleObject,
-  itemResponseHandler,
-});
-
 const getWidgets = async () => {
-  const response = await getter.getItem({});
-  return applyTransform(response.data, [
-    snakeToCamel(),
-  ]);
+  try {
+    const response = await userHelperService.activityWorkspaceWidget();
+    return applyTransform(response.data, [
+      snakeToCamel(),
+    ]);
+  } catch (err) {
+    throw applyTransform(err, [
+      notify,
+    ]);
+  }
 };
 
 const widgetsAPI = {
