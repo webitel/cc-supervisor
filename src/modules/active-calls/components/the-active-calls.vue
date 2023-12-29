7c6aee243cb4131039faf7f5949642225f0db4b8
Lera24 <bedulinavalera21@gmail.com>
2023-12-29 09:08:23 +0200

    hotfix: change dummy in darkMode[WTEL-4111]

--- src/modules/active-calls/components/the-active-calls.vue
+++ src/modules/active-calls/components/the-active-calls.vue
@@ -19,7 +19,6 @@
           v-if="dummyValue && !isLoading"
           :src="dummyValue.src"
           :text="dummyValue.text"
-          :size="300"
           class="main-section-wrapper__dummy"
         ></wt-dummy>
         <div class="table-wrapper" v-show="!isLoading && !dummyValue">
@@ -82,15 +81,17 @@
 
 <script>
 import sortFilterMixin from '@webitel/ui-sdk/src/mixins/dataFilterMixins/sortFilterMixin';
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 import tablePageMixin from '../../../app/mixins/supervisor-workspace/tablePageMixin';
 import FilterPagination from '../../_shared/filters/components/filter-pagination.vue';
 import FilterFields from '../../_shared/filters/components/filter-table-fields.vue';
 import ActiveCallsFilters from '../modules/filters/components/active-calls-filters.vue';
 import TableActiveCallState from './_internals/table-templates/table-active-call-state.vue';
 import TableDirection from './_internals/table-templates/table-direction.vue';
-import Dummy from '../assets/sv-dummy.svg';
-import DummyAfterSearch from '../assets/sv-dummy-after-search.svg';
+import DummyLight from '../assets/sv-dummy-light.svg';
+import DummyDark from '../assets/sv-dummy-dark.svg';
+import DummyAfterSearchLight from '../assets/sv-dummy-after-search-light.svg';
+import DummyAfterSearchDark from '../assets/sv-dummy-after-search-dark.svg';
 
 export default {
   name: 'the-active-calls',
@@ -109,16 +110,19 @@ export default {
     namespace: 'activeCalls',
   }),
   computed: {
+    ...mapGetters('appearance', {
+      darkMode: 'DARK_MODE',
+    }),
     dummyValue() {
       if (!this.dataList.length) {
         if (Object.values(this.$route.query).some((query) => query.length)) {
           return {
-            src: DummyAfterSearch,
+            src: this.darkMode ? DummyAfterSearchDark : DummyAfterSearchLight,
             text: this.$t('pages.activeCall.empty.resultSearch'),
           };
         }
         return {
-          src: Dummy,
+          src: this.darkMode ? DummyDark : DummyLight,
           text: this.$t('pages.activeCall.empty.workspace'),
         };
       }
