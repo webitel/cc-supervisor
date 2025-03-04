4604ab96c13b597a7fa73df80611a544693a08d0
Олександр Пальонний <s2567k@gmail.com>
2025-03-04 16:25:18 +0200

    set style fixes for main.scss file [WTEL-6433] (https://webitel.atlassian.net/browse/WTEL-6433)

--- src/app/components/the-supervisor-workspace.vue
+++ src/app/components/the-supervisor-workspace.vue
@@ -43,7 +43,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '../css/supervisor-workspace/the-supervisor-workspace';
+@use '../css/supervisor-workspace/the-supervisor-workspace';
 
 .the-supervisor-workspace {
   display: flex;
