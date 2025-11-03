d5ecef3d8b09b72fcd33978c0c75ee3903f49bcd
Oles Chorpita <chorpitaoles@gmail.com>
2025-11-03 23:51:45 +0200

    fix: fixed comment [WTEL-7926]

--- src/modules/agents/modules/agent-card/components/agent-panel/_internals/agent-status-comment.vue
+++ src/modules/agents/modules/agent-card/components/agent-panel/_internals/agent-status-comment.vue
@@ -25,7 +25,8 @@ defineProps({
 <style scoped lang="scss">
 .agent-panel__comment {
   word-break: break-word;
-  // TODO: Remove this once the issue in wt-tooltip is fixed (https://webitel.atlassian.net/browse/WTEL-7672)
+  //@author Oles Chorpita
+  // TODO: Remove max-width when wt-tooltip content will have max-width or add tooltip variable (https://webitel.atlassian.net/browse/WTEL-7672)
   // width restriction so that it doesn't stretch across the page view
   max-width: 280px;
 }
