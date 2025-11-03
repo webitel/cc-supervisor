d51e1eea508fcadeda105346457a6b18dd5b9b62
Oles Chorpita <chorpitaoles@gmail.com>
2025-11-03 14:20:34 +0200

    fix: added max width for comment [WTEL-7672](https://webitel.atlassian.net/browse/WTEL-7672)

--- src/modules/agents/modules/agent-card/components/agent-panel/_internals/agent-status-comment.vue
+++ src/modules/agents/modules/agent-card/components/agent-panel/_internals/agent-status-comment.vue
@@ -6,7 +6,9 @@
         color="info"
       />
     </template>
+    <p class="agent-panel__comment">
     {{ statusComment }}
+    </p>
   </wt-tooltip>
 </template>
 <script setup lang="ts">
@@ -21,5 +23,9 @@ defineProps({
 </script>
 
 <style scoped lang="scss">
-
+.agent-panel__comment {
+  word-break: break-word;
+  //width restriction so that it doesn't stretch across the page view
+  max-width: 280px;
+}
 </style>
