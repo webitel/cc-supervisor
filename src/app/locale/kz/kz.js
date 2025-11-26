ea6325b6416c5b4eeae7ef4cf34f41a4da713622
Yehor Hlukhov <egorgluhov68@gmail.com>
2025-11-26 17:51:08 +0200

    feat: add locales for pause causes [WTEL-8179](https://webitel.atlassian.net/browse/WTEL-8179)

--- src/app/locale/kz/kz.js
+++ src/app/locale/kz/kz.js
@@ -241,5 +241,12 @@ export default {
         unlimited: 'Шексіз',
       },
     },
+    pauseCauses: {
+      combinationPrefix: 'Жоқ ',
+      sip: 'SIP',
+      ws: 'websocket',
+      screen_control: 'DeskTrack',
+      no_cc_license: 'лицензия',
+    },
   },
 };
