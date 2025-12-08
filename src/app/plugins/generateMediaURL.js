c6f18ad2951dcdfed3f0853bb55d5235199cd453
Yehor Hlukhov <egorgluhov68@gmail.com>
2025-12-08 12:46:58 +0200

    refactor: media url generation [WTEL-8283](https://webitel.atlassian.net/browse/WTEL-8283)

--- src/app/plugins/generateMediaURL.js
+++
@@ -1,8 +0,0 @@
-const token = localStorage.getItem('access-token');
-const BASE_URL = import.meta.env.VITE_API_URL;
-
-const generateMediaURL = (id) => (
-  `${BASE_URL}/storage/recordings/${id}/stream?access_token=${token}`
-);
-
-export default generateMediaURL;
