36fcc32a83219bb7f608c757877e906a85aa6706
Yehor Hlukhov <egorgluhov68@gmail.com>
2025-12-11 15:40:47 +0200

    refactor: optimize rated parameter in getAgentCallsList [WTEL-8214](https://webitel.atlassian.net/browse/WTEL-8214)

--- src/modules/agents/modules/agent-card/modules/agent-calls/api/agent-calls.js
+++ src/modules/agents/modules/agent-card/modules/agent-calls/api/agent-calls.js
@@ -108,7 +108,7 @@ const getAgentCallsList = async ({ options, ...params }) => {
     hasTranscript,
     number,
     missed,
-    rated: rated === 'true' ? true : rated === 'false' ? false : rated,
+    rated: rated === 'true',
     ratedBy,
     domainId,
   };
