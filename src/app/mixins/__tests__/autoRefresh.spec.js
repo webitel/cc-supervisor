daf9e8c2d3611edc77cbc13829f442d7881bc1b2
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2026-06-09 18:48:55 +0300

    test: enhance autoRefresh test by unmounting wrapper to prevent interval issues [WTEL-9502](https://webitel.atlassian.net/browse/WTEL-9502)

--- src/app/mixins/__tests__/autoRefresh.spec.js
+++ src/app/mixins/__tests__/autoRefresh.spec.js
@@ -21,10 +21,15 @@ test('autoRefresh triggered function twice', async () => {
 			this.setAutoRefresh();
 		},
 	};
-	shallowMount(Component);
+	const wrapper = shallowMount(Component);
 	await new Promise((r) =>
 		setTimeout(() => {
 			r();
 		}, 20),
 	);
+	// Unmount to clear the auto-refresh interval; otherwise it keeps firing after
+	// the test ends and throws `document is not defined` once happy-dom tears the
+	// environment down.
+	wrapper.unmount();
+	expect(counter).toBeGreaterThan(0);
 });
